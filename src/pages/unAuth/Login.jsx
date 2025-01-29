7e9278771bf2c53d52ec829e43ab211a0607d772
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-01-29 11:03:39 +0700

    Feat: - add animation transition while hover continue - add condition if email not valid, button cannot clicked

--- src/pages/unAuth/Login.jsx
+++ src/pages/unAuth/Login.jsx
@@ -1,8 +1,18 @@
 import { GoogleLogin } from "@react-oauth/google";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { validate } from "react-email-validator";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+    setIsValidEmail(validate(email));
+  };
+
   const navigate = useNavigate();
 
   return (
@@ -21,25 +31,40 @@ export default function Login() {
             </label>
             <input
               type="email"
-              id="email"
-              name="email"
+              value={email}
+              onChange={handleEmailChange}
               className="mt-4 w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none outline-none focus:border-blue-500"
-              placeholder="Enter your email"
+              placeholder="id@email.com"
             />
           </div>
-          <div className="mt-4">
-            <button className="rounded-4xl border border-gray-300 cursor-pointer font-semibold text-blue-500 w-full py-2 hover:border-blue-500">
-              Continue
-            </button>
-          </div>
+          {isValidEmail ? (
+            <div className="mt-4">
+              <button className="rounded-4xl border border-gray-300 transition-all duration-500 cursor-pointer font-semibold hover:bg-blue-50 text-blue-500 w-full py-2 hover:border-blue-500">
+                Continue
+              </button>
+            </div>
+          ) : (
+            <div className="mt-4">
+              <button className="rounded-4xl border border-gray-300 font-semibold text-gray-300 w-full py-2 bg-blue-50">
+                Continue
+              </button>
+            </div>
+          )}
+
           <div className="my-5 grid grid-cols-3 items-center text-gray-500 text-xs md:text-base">
             <hr className=" border-gray-300" />
             <p className="text-center">OR</p>
             <hr className=" border-gray-300" />
           </div>
           <div className="flex justify-center font-normal">
-            <div id="buttonDiv"></div>
             <GoogleLogin />
+          </div>
+          <div>
+            <p className="text-xs text-gray-500 mt-4 text-center">
+              By signing in, I agree to Agoda&apos;s{" "}
+              <span className="text-blue-500">Terms of Use</span> and{" "}
+              <span className="text-blue-500">Privacy Policy.</span>
+            </p>
           </div>
         </div>
       </div>
