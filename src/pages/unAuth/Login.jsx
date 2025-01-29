1a8a7b31ad9cfdb85a55d44d5d4e9d658a778306
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-01-29 23:26:32 +0700

    feat: add responsive on login page

--- src/pages/unAuth/Login.jsx
+++ src/pages/unAuth/Login.jsx
@@ -29,33 +29,36 @@ export default function Login() {
   }, [input]);
 
   return (
-    <div className="mx-auto xl:w-[550px] flex justify-center items-center h-screen">
-      <div className="border border-gray-100 rounded-sm shadow-xl h-[920px] p-10 w-full">
-        <>
+    <div className="mx-auto w-full px-4 sm:px-6 md:px-8 lg:px-0 lg:w-[550px] flex justify-center items-center min-h-screen py-4">
+      <div className="border border-gray-100 rounded-lg shadow-xl w-full p-4 sm:p-6 md:p-8 lg:p-10">
+        <div className="space-y-6">
           <div>
-            <p className="text-xl font-semibold">Sign in</p>
-            <p className="text-sm">
+            <h1 className="text-xl sm:text-2xl font-semibold">Sign in</h1>
+            <p className="text-xs sm:text-sm text-gray-500">
               Sign up for free or log in to access amazing deals and benefits!
             </p>
           </div>
-          <div>
+
+          <div className="w-full md:max-w-[300px] max-w-[200px] mx-auto">
             <Lottie animationData={authAnimation} loop={true} />
           </div>
-          <div>
+
+          <div className="md:space-y-4 space-y-2">
             <div className="group relative">
-              <label className="absolute left-3.5 top-1.5 text-sm bg-white px-1 text-gray-500 group-focus-within:text-blue-500">
+              <label className="absolute left-3.5 top-1.5 text-xs sm:text-sm bg-white px-1 text-gray-500 group-focus-within:text-blue-500">
                 Email
               </label>
               <input
                 type="email"
                 value={input.email}
                 onChange={(e) => setInput({ ...input, email: e.target.value })}
-                className="mt-4 w-full border text-sm border-gray-300 rounded-lg px-4 py-3 focus:outline-none outline-none focus:border-blue-500"
+                className="mt-4 w-full border text-sm border-gray-300 rounded-lg px-4 md:py-3 py-2 focus:outline-none focus:border-blue-500"
                 placeholder="id@email.com"
               />
             </div>
-            <div className="group relative mt-3">
-              <label className="absolute left-3.5 top-1.5 text-sm bg-white px-1 text-gray-500 group-focus-within:text-blue-500">
+
+            <div className="group relative">
+              <label className="absolute left-3.5 top-1.5 text-xs sm:text-sm bg-white px-1 text-gray-500 group-focus-within:text-blue-500">
                 Password
               </label>
               <input
@@ -64,44 +67,48 @@ export default function Login() {
                 onChange={(e) =>
                   setInput({ ...input, password: e.target.value })
                 }
-                className="mt-4 w-full border text-sm border-gray-300 rounded-lg px-4 py-3 focus:outline-none outline-none focus:border-blue-500"
+                className="mt-4 w-full border text-sm border-gray-300 rounded-lg px-4 md:py-3 py-2 focus:outline-none focus:border-blue-500"
                 placeholder="Password"
               />
             </div>
-            {isValidInputLogin ? (
-              <div className="mt-4">
+
+            <div>
+              {isValidInputLogin ? (
                 <button
-                  className="rounded-4xl border border-gray-300 transition-all duration-500 cursor-pointer font-semibold hover:bg-blue-50 text-blue-500 w-full py-2 hover:border-blue-500"
+                  className="rounded-4xl border border-gray-300 transition-all duration-300 cursor-pointer font-semibold hover:bg-blue-50 text-blue-500 w-full py-1.5 sm:py-2.5 hover:border-blue-500"
                   onClick={handleLogin}
                 >
                   Login
                 </button>
-              </div>
-            ) : (
-              <div className="mt-4">
-                <button className="rounded-4xl border border-gray-300 font-semibold text-gray-300 w-full py-2 bg-blue-50">
+              ) : (
+                <button className="rounded-4xl border border-gray-300 font-semibold text-gray-300 w-full py-1.5 sm:py-2.5 bg-blue-50">
                   Login
                 </button>
-              </div>
-            )}
+              )}
+            </div>
 
-            <div className="my-5 grid grid-cols-5 items-center text-gray-500 text-xs md:text-base">
+            <div className="grid grid-cols-5 items-center text-gray-500 text-xs sm:text-sm">
               <hr className="col-span-2 border-gray-300" />
               <p className="text-center">OR</p>
               <hr className="col-span-2 border-gray-300" />
             </div>
-            <div className="flex justify-center font-normal">
+
+            <div className="flex justify-center">
               <GoogleLogin />
             </div>
-            <div>
-              <p className="text-xs text-gray-500 mt-4 text-center">
-                By signing in, I agree to Agoda&apos;s{" "}
-                <span className="text-blue-500">Terms of Use</span> and{" "}
-                <span className="text-blue-500">Privacy Policy.</span>
-              </p>
-            </div>
+
+            <p className="text-xs sm:text-sm text-gray-500 text-center">
+              By signing in, I agree to Agoda&apos;s{" "}
+              <span className="text-blue-500 cursor-pointer hover:underline">
+                Terms of Use
+              </span>{" "}
+              and{" "}
+              <span className="text-blue-500 cursor-pointer hover:underline">
+                Privacy Policy.
+              </span>
+            </p>
           </div>
-        </>
+        </div>
       </div>
     </div>
   );
