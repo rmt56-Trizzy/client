7ba317c414b722eabf01ab74f3075c9e4eba2eb3
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-01 01:57:07 +0700

    feat: add website detail

--- src/components/places/TwoTopPlaceCards.jsx
+++ src/components/places/TwoTopPlaceCards.jsx
@@ -1,18 +1,20 @@
 export default function TwoTopPlaceCards() {
   return (
-    <div className="relative">
-      <div className="absolute top-0 rounded-xl left-0 md:w-full h-full bg-gradient-to-b from-blue-500/45 to-transparent z-10 w-[165px]"></div>
+    <div className="relative h-full">
+      <div className="absolute top-0 rounded-xl left-0 w-full h-full bg-gradient-to-b from-blue-500/45 to-transparent z-10"></div>
+
       <img
         src="https://cf.bstatic.com/xdata/images/city/600x600/688053.jpg?k=da426cecdc6492da255ca0612c4bb41bd785b2565c83405f7281f15c05b16376&o="
         alt="jakarta"
-        className="rounded-xl lg:h-[270px] lg:w-[540px] md:h-[190px] md:w-[380px] w-[165px] h-[100px] object-cover"
+        className="rounded-xl w-full h-full object-cover"
       />
-      <div className="absolute md:top-2 md:p-5 top-0 p-1 flex md:gap-2 gap-1 items-center z-20">
+
+      <div className="absolute md:top-2 md:py-3 md:px-5 top-0 p-1 flex md:gap-2 gap-1 items-center z-20">
         <p className="font-bold text-white md:text-2xl text-sm">Jakarta</p>
         <img
           src="/img/Indonesia flag.png"
           alt="indonesia-flag"
-          className="w-2.5 h-2 md:w-[24px] md:h-[16px] md:mt-2"
+          className="w-2.5 h-2 md:w-[24px] md:h-[16px] md:mt-1"
         />
       </div>
     </div>
--- src/pages/unAuth/Homepage.jsx
+++ src/pages/unAuth/Homepage.jsx
@@ -1,6 +1,6 @@
 import CardChatBox from "../../components/AI/CardChatBox";
-import ThreeTopPlace from "../../components/places/ThreeTopPlace";
 import TwoTopPlaceCards from "../../components/places/TwoTopPlaceCards";
+import { MdOutlineFlightTakeoff } from "react-icons/md";
 
 export default function Homepage() {
   return (
@@ -16,22 +16,44 @@ export default function Homepage() {
           Top Places to Visit
         </p>
         <div className="grid grid-cols-2 md:gap-5 gap-2 mt-5">
-          <div className="col-span-1">
+          <div className="col-span-1 lg:h-[270px] md:h-[190px] h-[100px]">
             <TwoTopPlaceCards />
           </div>
-          <div className="col-span-1">
+          <div className="col-span-1 lg:h-[270px] md:h-[190px] h-[100px]">
             <TwoTopPlaceCards />
           </div>
         </div>
         <div className="grid grid-cols-3 md:gap-5 gap-2 md:mt-5 mt-2">
-          <div className="col-span-1">
-            <ThreeTopPlace />
+          <div className="col-span-1 lg:h-[270px] md:h-[190px] h-[90px]">
+            <TwoTopPlaceCards />
           </div>
-          <div className="col-span-1">
-            <ThreeTopPlace />
+          <div className="col-span-1 lg:h-[270px] md:h-[190px] h-[90px]">
+            <TwoTopPlaceCards />
           </div>
-          <div className="col-span-1">
-            <ThreeTopPlace />
+          <div className="col-span-1 lg:h-[270px] md:h-[190px] h-[90px]">
+            <TwoTopPlaceCards />
+          </div>
+        </div>
+        <div className="mt-10 md:mt-20 lg:mt-36 md:text-5xl lg:text-6xl text-2xl font-semibold text-center text-slate-700">
+          <p className="font-bold">
+            Simplify your travel using our trip planner AI
+          </p>
+          <p className="font-base md:text-base text-xs md:mt-10 mt-5 md:px-10">
+            Meet Trizzy, your ultimate AI travel assistant. From finding the
+            perfect hotel to crafting the ideal itinerary, Trizzy makes planning
+            your trips seamless. No more juggling between hotel booking sites
+            and itinerary planners – Trizzy is all you need. Chat with Trizzy to
+            receive personalized hotel recommendations based on your
+            preferences, and get inspired with unique itinerary suggestions.
+            Then, create your custom travel plan and make the most of every
+            moment at the best hotels with Trizzy. Planning your dream vacation
+            has never been easier!
+          </p>
+          <div className="flex rounded-lg md:w-[330px] w-[180px] md:mt-10 mt-5 bg-slate-700 text-white mx-auto gap-3 justify-center items-center">
+            <button className="md:py-3 py-2.5 md:text-2xl text-sm">
+              Plan a new trip
+            </button>
+            <MdOutlineFlightTakeoff className="md:text-3xl text-2xl" />
           </div>
         </div>
       </div>
