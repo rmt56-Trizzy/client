1d2671dbf70c45643b4d3c5e1fb1490a5c845167
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-01 01:01:20 +0700

    feat: add card for top 3 place to visits

--- src/pages/unAuth/Homepage.jsx
+++ src/pages/unAuth/Homepage.jsx
@@ -1,4 +1,5 @@
 import CardChatBox from "../../components/AI/CardChatBox";
+import ThreeTopPlace from "../../components/places/ThreeTopPlace";
 import TwoTopPlaceCards from "../../components/places/TwoTopPlaceCards";
 
 export default function Homepage() {
@@ -10,14 +11,27 @@ export default function Homepage() {
           <CardChatBox />
         </div>
       </div>
-      <div className="mt-24 md:mx-auto lg:w-[1000px] md:w-[750px] px-4 md:px-0">
-        <p className="text-3xl font-semibold">Top Places to Visit</p>
+      <div className="md:mt-24 mt-18 md:mx-auto lg:w-[1000px] md:w-[750px] px-4 md:px-0">
+        <p className="lg:text-3xl md:text-2xl font-semibold">
+          Top Places to Visit
+        </p>
         <div className="grid grid-cols-2 md:gap-5 gap-2 mt-5">
           <div className="col-span-1">
             <TwoTopPlaceCards />
           </div>
           <div className="col-span-1">
             <TwoTopPlaceCards />
+          </div>
+        </div>
+        <div className="grid grid-cols-3 md:gap-5 gap-2 md:mt-5 mt-2">
+          <div className="col-span-1">
+            <ThreeTopPlace />
+          </div>
+          <div className="col-span-1">
+            <ThreeTopPlace />
+          </div>
+          <div className="col-span-1">
+            <ThreeTopPlace />
           </div>
         </div>
       </div>
