bf47764566d8c0d68dc5ef880764b3ef4033237d
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-01-31 08:56:55 +0700

    feat: add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,31 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const isLogin = false;
+  return (
+    <div className=" md:h-[67px] border-b-1 border-gray-300">
+      <div className="mx-auto container flex items-center h-full">
+        <div className="me-auto">LOGO</div>
+        <div>
+          {isLogin ? (
+            <div className="flex items-center gap-2">
+              <img
+                src="/img/profile.png"
+                alt="profile-image"
+                className="w-9 h-9 rounded-full"
+              />
+              <p className="text-xs lg:text-sm">Profile Name</p>
+            </div>
+          ) : (
+            <div className="flex gap-2">
+              <button className="rounded-md border border-green-600 py-1 px-3 font-semibold hover:bg-green-50 text-green-600">
+                Login
+              </button>
+              <button className="rounded-md border border-blue-600 py-1 px-3 font-semibold hover:bg-blue-50 text-blue-600">
+                Register
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
