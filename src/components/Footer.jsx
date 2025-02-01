488e4b5722e41efb05ba76defd21f567bb2fe76b
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-01 13:36:55 +0700

    feat: change padding and margin on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ import { NavLink } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="bg-white text-gray-900 py-10 border-t border-gray-200">
+    <footer className="bg-white text-gray-900 md:py-10 py-6 border-t border-gray-200">
       <div className="container mx-auto px-6 md:px-12 lg:px-20">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:py-10 md:py-6 md:grid-cols-3 gap-8">
           <div>
-            <h2 className="md:text-3xl text-xl text-slate-700 font-bold">
+            <h2 className="md:text-4xl lg:text-5xl text-2xl text-slate-700 font-bold">
               Trizzy
             </h2>
             <p className="mt-3 text-gray-600 text-sm md:text-base">
@@ -59,36 +59,36 @@ export default function Footer() {
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="w-full px-4 py-2 text-sm text-gray-900 border border-teal-300 rounded-l-md focus:outline-none outline-none"
+                className="w-full px-4 py-2 text-sm lg:text-sm md:text-xs text-gray-900 border border-teal-300 rounded-l-md focus:outline-none outline-none"
               />
-              <button className="bg-teal-500 text-white px-4 py-2 rounded-r-md hover:bg-teal-600 cursor-pointer">
+              <button className="bg-teal-500 text-white md:px-3 lg:px-4 px-4 py-2 md:text-xs lg:text-base rounded-r-md hover:bg-teal-600 cursor-pointer">
                 Subscribe
               </button>
             </div>
           </div>
         </div>
 
-        <div className="mt-10 flex flex-col md:flex-row justify-between items-center border-t border-gray-300 pt-6">
-          <p className="text-gray-600 text-sm">
+        <div className="mt-10 flex flex-col md:flex-row justify-between text-xl md:text-2xl lg:text-3xl items-center border-t border-gray-300 pt-6">
+          <p className="text-gray-600 lg:text-base text-sm">
             &copy; 2025 Trizzy. All rights reserved.
           </p>
-          <div className="flex space-x-4 mt-4 md:mt-0 text-xl">
-            <button className="text-gray-600 hover:text-blue-500">
+          <div className="flex space-x-4 mt-4 md:mt-0 ">
+            <button className="text-gray-600 hover:text-teal-500">
               <FaFacebook />
             </button>
-            <button className="text-gray-600 hover:text-blue-500">
+            <button className="text-gray-600 hover:text-teal-500">
               <FaXTwitter />
             </button>
-            <button className="text-gray-600 hover:text-blue-500">
+            <button className="text-gray-600 hover:text-teal-500">
               <FaInstagram />
             </button>
             <NavLink
               to="https://github.com/rmt56-Trizzy"
-              className="text-gray-600 hover:text-blue-500"
+              className="text-gray-600 hover:text-teal-500"
             >
               <FaGithub />
             </NavLink>
-            <button className="text-gray-600 hover:text-blue-500">
+            <button className="text-gray-600 hover:text-teal-500">
               <FaLinkedin />
             </button>
           </div>
