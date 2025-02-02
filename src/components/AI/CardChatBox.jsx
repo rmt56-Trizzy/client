2de5eee00b451bf2fcef66ebd40de0a5a11aded7
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-03 01:44:07 +0700

    feat: connect button ask ai to chat page

--- src/components/AI/CardChatBox.jsx
+++ src/components/AI/CardChatBox.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import { useTypewriter } from "react-simple-typewriter";
 
 export default function CardChatBox() {
@@ -11,12 +12,18 @@ export default function CardChatBox() {
     delaySpeed: 2000,
   });
 
+  const navigate = useNavigate();
+
   const textSuggestion = [
     "Inspire me where to go",
     "Create a new Trip",
     "Find family hotels in Dubai",
     "Summer Trip",
   ];
+
+  const handleAskAi = () => {
+    navigate("/chat");
+  };
   return (
     <div className="rounded-xl shadow-lg bg-white w-[305px] h-[100px] md:w-[618px] md:h-[154px] p-2.5">
       <div className="flex justify-between items-center">
@@ -25,7 +32,10 @@ export default function CardChatBox() {
           alt="profile"
           className="md:h-5.5 md:w-5.5 w-5 h-5"
         />
-        <button className="px-2 py-1.5 cursor-pointer bg-teal-500 text-xs md:text-sm rounded-md text-white ">
+        <button
+          className="px-2 py-1.5 cursor-pointer bg-teal-500 text-xs md:text-sm rounded-md text-white"
+          onClick={handleAskAi}
+        >
           Ask Anything
         </button>
       </div>
