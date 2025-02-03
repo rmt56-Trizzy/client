bd4af191ffebc2700d804c612826c56ad60cfa90
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-02-03 16:25:19 +0700

    feat: add animation on chat page

--- src/pages/Auth/ChatPage.jsx
+++ src/pages/Auth/ChatPage.jsx
@@ -1,14 +1,27 @@
 import TopPlaceCard from "../../components/places/TopPlaceCard";
 import ChatBox from "../../components/AI/ChatBox";
+import { motion } from "framer-motion";
 
 export default function ChatPage() {
   return (
     <div className="flex lg:flex-row flex-col md:mx-auto lg:container px-4 lg:pb-20 py-5 md:pb-0 min-h-[95vh] lg:min-h-0 md:px-0 justify-center lg:my-0 md:mb-2">
-      <div className="lg:w-1/3 md:w-[500px] px-4 lg:mx-0 justify-center flex items-center md:mx-auto">
+      <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.3 }}
+        viewport={{ once: true }}
+        className="lg:w-1/3 md:w-[500px] px-4 lg:mx-0 justify-center flex items-center md:mx-auto"
+      >
         <ChatBox />
-      </div>
+      </motion.div>
 
-      <div className="lg:w-2/3 mx-auto  px-4 w-[280px] md:w-[700px] justify-center lg:flex-col">
+      <motion.div
+        initial={{ opacity: 0, x: 200 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.3 }}
+        viewport={{ once: true }}
+        className="lg:w-2/3 mx-auto  px-4 w-[280px] md:w-[700px] justify-center lg:flex-col"
+      >
         <h2 className="md:text-3xl text-xl my-4 font-bold md:my-10 text-center text-slate-700">
           Destination
         </h2>
@@ -23,7 +36,7 @@ export default function ChatPage() {
             </div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
